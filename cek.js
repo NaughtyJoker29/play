f26d9276867643dc8c2420905ac73d54edd130dc
NaughtyJoker29 <107688568+NaughtyJoker29@users.noreply.github.com>
2023-03-29 11:41:29 +0700

    Create cek.js

---
+++ cek.js
@@ -0,0 +1,216 @@
+<script>//<![CDATA[
+
+/* SETTING COUNTDOWN */
+var countDownDate1 = new Date("Mar 29, 2023 23:45:00").getTime();
+var countDownDate2 = new Date("Mar 30, 2023 02:00:00").getTime();
+var countDownDate3 = new Date("Mar 30, 2023 23:45:00").getTime();
+var countDownDate4 = new Date("Mar 31, 2023 02:00:00").getTime();
+var countDownDate5 = new Date("Apr 1, 2023 01:30:00").getTime();
+var countDownDate6 = new Date("Apr 1, 2023 02:00:00").getTime();
+var countDownDate7 = new Date("Apr 1, 2023 02:00:00").getTime();
+var countDownDate8 = new Date("Apr 1, 2023 18:30:00").getTime();
+var countDownDate9 = new Date("Apr 1, 2023 20:00:00").getTime();
+var countDownDate10 = new Date("Apr 1, 2023 20:00:00").getTime();
+var countDownDate11 = new Date("Apr 1, 2023 20:00:00").getTime();
+var countDownDate12 = new Date("Apr 1, 2023 20:00:00").getTime();
+var countDownDate13 = new Date("Apr 1, 2023 20:30:00").getTime();
+var countDownDate14 = new Date("Apr 1 2023 20:30:00").getTime();
+var countDownDate15 = new Date("Apr 1, 2023 20:30:00").getTime();
+var countDownDate16 = new Date("Apr 1, 2023 20:30:00").getTime();
+var countDownDate17 = new Date("Apr 1, 2023 21:00:00").getTime();
+var countDownDate18 = new Date("Apr 1, 2023 21:00:00").getTime();
+var countDownDate19 = new Date("Apr 1, 2023 21:00:00").getTime();
+var countDownDate20 = new Date("Apr 1, 2023 21:00:00").getTime();
+var countDownDate21 = new Date("Apr 1, 2023 21:00:00").getTime();
+var countDownDate22 = new Date("Apr 1, 2023 21:00:00").getTime();
+var countDownDate23 = new Date("Apr 1, 2023 21:15:00").getTime();
+var countDownDate24 = new Date("Apr 1, 2023 21:30:00").getTime();
+var countDownDate25 = new Date("Apr 1, 2023 22:00:00").getTime();
+var countDownDate26 = new Date("Apr 1, 2023 23:00:00").getTime();
+var countDownDate27 = new Date("Apr 1, 2023 23:30:00").getTime();
+var countDownDate28 = new Date("Apr 1, 2023 23:30:00").getTime();
+var countDownDate29 = new Date("Apr 1, 2023 23:30:00").getTime();
+var countDownDate30 = new Date("Apr 1, 2023 23:45:00").getTime();
+var countDownDate31 = new Date("Apr 2, 2023 00:30:00").getTime();
+var countDownDate32 = new Date("Apr 2, 2023 01:00:00").getTime();
+var countDownDate33 = new Date("Apr 2, 2023 01:00:00").getTime();
+var countDownDate34 = new Date("Apr 2, 2023 01:45:00").getTime();
+var countDownDate35 = new Date("Apr 2, 2023 02:00:00").getTime();
+var countDownDate36 = new Date("Apr 2, 2023 02:00:00").getTime();
+var countDownDate37 = new Date("Apr 2, 2023 02:00:00").getTime();
+var countDownDate38 = new Date("Apr 2, 2023 22:15:00").getTime();
+var countDownDate39 = new Date("Apr 2, 2023 23:00:00").getTime();
+var countDownDate40 = new Date("Apr 2, 2023 06:30:00").getTime();
+var countDownDate41 = new Date("Mar 28, 2023 07:30:00").getTime();
+var countDownDate42 = new Date("Mar 5, 2023 00:30:00").getTime();
+var countDownDate43 = new Date("Mar 5, 2023 00:30:00").getTime();
+var countDownDate44 = new Date("Mar 5, 2023 00:30:00").getTime();
+var countDownDate45 = new Date("Mar 5, 2023 00:45:00").getTime();
+var countDownDate46 = new Date("Mar 5, 2023 02:00:00").getTime();
+var countDownDate47 = new Date("Mar 5, 2023 02:45:00").getTime();
+var countDownDate48 = new Date("Mar 5, 2023 03:00:00").getTime();
+var countDownDate49 = new Date("Mar 5, 2023 03:00:00").getTime();
+var countDownDate50 = new Date("Mar 11, 2023 07:00:00").getTime();
+var countDownDate51 = new Date("Mar 24, 2023 06:00:00").getTime();
+var countDownDate52 = new Date("Mar 26, 2023 07:00:00").getTime();
+var countDownDate53 = new Date("Mar 26, 2023 08:00:00").getTime();
+var countDownDate54 = new Date("Mar 26, 2023 09:00:00").getTime();
+var countDownDate55 = new Date("Mar 26, 2023 09:00:00").getTime();
+var countDownDate56 = new Date("Mar 26, 2023 09:30:00").getTime();
+var countDownDate57 = new Date("Mar 8, 2023 10:00:00").getTime();
+var countDownDate58 = new Date("Mar 8, 2023 10:00:00").getTime();
+var countDownDate59 = new Date("Mar 4, 2023 10:00:00").getTime();
+var countDownDate60 = new Date("Mar 25, 2023 21:00:00").getTime();
+var countDownDate61 = new Date("Mar 25, 2023 27:00:00").getTime();
+var countDownDate62 = new Date("Mar 4, 2023 03:00:00").getTime();
+var countDownDate63 = new Date("Mar 4, 2023 03:00:00").getTime();
+var countDownDate64 = new Date("Mar 4, 2023 03:00:00").getTime();
+var countDownDate65 = new Date("Mar 5, 2023 03:00:00").getTime();
+
+
+var timer1 = document.getElementById("timer1");
+var timer2 = document.getElementById("timer2");
+var timer3 = document.getElementById("timer3");
+var timer4 = document.getElementById("timer4");
+var timer5 = document.getElementById("timer5");
+var timer6 = document.getElementById("timer6");
+var timer7 = document.getElementById("timer7");
+var timer8 = document.getElementById("timer8");
+var timer9 = document.getElementById("timer9");
+var timer10 = document.getElementById("timer10");
+var timer11 = document.getElementById("timer11");
+var timer12 = document.getElementById("timer12");
+var timer13 = document.getElementById("timer13");
+var timer14 = document.getElementById("timer14");
+var timer15 = document.getElementById("timer15");
+var timer16 = document.getElementById("timer16");
+var timer17 = document.getElementById("timer17");
+var timer18 = document.getElementById("timer18");
+var timer19 = document.getElementById("timer19");
+var timer20 = document.getElementById("timer20");
+var timer21 = document.getElementById("timer21");
+var timer22 = document.getElementById("timer22");
+var timer23 = document.getElementById("timer23");
+var timer24 = document.getElementById("timer24");
+var timer25 = document.getElementById("timer25");
+var timer26 = document.getElementById("timer26");
+var timer27 = document.getElementById("timer27");
+var timer28 = document.getElementById("timer28");
+var timer29 = document.getElementById("timer29");
+var timer30 = document.getElementById("timer30");
+var timer31 = document.getElementById("timer31");
+var timer32 = document.getElementById("timer32");
+var timer33 = document.getElementById("timer33");
+var timer34 = document.getElementById("timer34");
+var timer35 = document.getElementById("timer35");
+var timer36 = document.getElementById("timer36");
+var timer37 = document.getElementById("timer37");
+var timer38 = document.getElementById("timer38");
+var timer39 = document.getElementById("timer39");
+var timer40 = document.getElementById("timer40");
+var timer41 = document.getElementById("timer41");
+var timer42 = document.getElementById("timer42");
+var timer43 = document.getElementById("timer43");
+var timer44 = document.getElementById("timer44");
+var timer45 = document.getElementById("timer45");
+var timer46 = document.getElementById("timer46");
+var timer47 = document.getElementById("timer47");
+var timer48 = document.getElementById("timer48");
+var timer49 = document.getElementById("timer49");
+var timer50 = document.getElementById("timer50");
+var timer51 = document.getElementById("timer51");
+var timer52 = document.getElementById("timer52");
+var timer53 = document.getElementById("timer53");
+var timer54 = document.getElementById("timer54");
+var timer55 = document.getElementById("timer55");
+var timer56 = document.getElementById("timer56");
+var timer57 = document.getElementById("timer57");
+var timer58 = document.getElementById("timer58");
+var timer59 = document.getElementById("timer59");
+var timer60 = document.getElementById("timer60");
+var timer61 = document.getElementById("timer61");
+
+
+
+/* COUNTDOWN */
+function countdown(finish_date, timer){
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = finish_date - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+timer.innerHTML = days + " D : " + hours + " H : "+ minutes + " M : " + seconds + " S ";
+if (distance < 0) {
+clearInterval(x);
+timer.innerHTML = "<livenow data-text='Live'>Live";
+}
+}, 1000);
+}
+
+
+/* SETTING START COUNTDOWN */
+countdown(countDownDate1, timer1)
+countdown(countDownDate2, timer2)
+countdown(countDownDate3, timer3)
+countdown(countDownDate4, timer4)
+countdown(countDownDate5, timer5)
+countdown(countDownDate6, timer6)
+countdown(countDownDate7, timer7)
+countdown(countDownDate8, timer8)
+countdown(countDownDate9, timer9)
+countdown(countDownDate10, timer10)
+countdown(countDownDate11, timer11)
+countdown(countDownDate12, timer12)
+countdown(countDownDate13, timer13)
+countdown(countDownDate14, timer14)
+countdown(countDownDate15, timer15)
+countdown(countDownDate16, timer16)
+countdown(countDownDate17, timer17)
+countdown(countDownDate18, timer18)
+countdown(countDownDate19, timer19)
+countdown(countDownDate20, timer20)
+countdown(countDownDate21, timer21)
+countdown(countDownDate22, timer22)
+countdown(countDownDate23, timer23)
+countdown(countDownDate24, timer24)
+countdown(countDownDate25, timer25)
+countdown(countDownDate26, timer26)
+countdown(countDownDate27, timer27)
+countdown(countDownDate28, timer28)
+countdown(countDownDate29, timer29)
+countdown(countDownDate30, timer30)
+countdown(countDownDate31, timer31)
+countdown(countDownDate32, timer32)
+countdown(countDownDate33, timer33)
+countdown(countDownDate34, timer34)
+countdown(countDownDate35, timer35)
+countdown(countDownDate36, timer36)
+countdown(countDownDate37, timer37)
+countdown(countDownDate38, timer38)
+countdown(countDownDate39, timer39)
+countdown(countDownDate40, timer40)
+countdown(countDownDate41, timer41)
+countdown(countDownDate42, timer42)
+countdown(countDownDate43, timer43)
+countdown(countDownDate44, timer44)
+countdown(countDownDate45, timer45)
+countdown(countDownDate46, timer46)
+countdown(countDownDate47, timer47)
+countdown(countDownDate48, timer48)
+countdown(countDownDate49, timer49)
+countdown(countDownDate50, timer50)
+countdown(countDownDate51, timer51)
+countdown(countDownDate52, timer52)
+countdown(countDownDate53, timer53)
+countdown(countDownDate54, timer54)
+countdown(countDownDate55, timer55)
+countdown(countDownDate56, timer56)
+countdown(countDownDate57, timer57)
+countdown(countDownDate58, timer58)
+countdown(countDownDate59, timer59)
+countdown(countDownDate60, timer60)
+countdown(countDownDate61, timer61)
+
+//]]></script>
